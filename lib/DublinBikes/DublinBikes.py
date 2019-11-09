fff5d60cae97ad87b9b8f8b52937d889e8f29a1a
Bhargav Panth <bhargavrpanth@gmail.com>
2019-11-09 21:15:05 +0000

    key str

--- lib/DublinBikes/DublinBikes.py
+++ lib/DublinBikes/DublinBikes.py
@@ -5,7 +5,7 @@ import json
 class DublinBikes(object):
 	def __init__(self):
 		self._url = 'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&'
-		self.val = "eae2a42f57ee0e99c46335e5960c1619eea2ded4"
+		self.val = 'eae2a42f57ee0e99c46335e5960c1619eea2ded4'
 		self.query_parameter = { 'apiKey' : self.val}
 		self.url = self._url + urllib.parse.urlencode(self.query_parameter)
 
